eb29ddee18824c75da9afb1d43781470265e7c23
thomas <silvathomas581@gmail.com>
2025-11-02 23:03:18 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descrição, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ThomasSilvadevloop.png" />
-    <Titulo fontSize={20}>Thomas Silva</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Thomas Silva</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        ThomasSilvadevloop
+      </Paragrafo>
+      <Descrição tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descrição>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
