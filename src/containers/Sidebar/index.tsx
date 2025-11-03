f930e0a351b07c7ba9a7eda3ed93309b2c8acda9
thomas <silvathomas581@gmail.com>
2025-11-02 21:22:05 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/ThomasSilvadevloop.png" />
+    <Titulo fontSize={20}>Thomas Silva</Titulo>
+  </aside>
+)
+
+export default Sidebar
